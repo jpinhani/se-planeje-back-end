452c0928680b109019319b150e0d771c695b4b71
jpinhani <jp.inhani@hotmail.com>
2020-03-19 19:26:54 -0300

    publish website

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 const app = require('./app')
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const hostname = 'http://seplaneje-com.umbler.net';
 
 
-app.listen(port, hostname, () => {
-  console.log(`App running running at http://${hostname}:${port}`);
+app.listen(port, () => {
+  console.log(`App running running at http://${port}`);
 
 })
