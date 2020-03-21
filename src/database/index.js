09dd8580b0aa4ace01673fbe2c3e0350d3bfc433
jpinhani <jp.inhani@hotmail.com>
2020-03-21 20:32:18 -0300

    Ajuste Delete Body

--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,7 @@ const mysql = require('mysql')
 
 module.exports = mysql.createConnection({
   host: 'mysql669.umbler.com',
-  // port     : 41890,
+  port: 41890,
   user: 'sysplaneje',
   password: 'master123',
   database: 'bdsplan',
