9372f0fe28d69febd8ac97602c3691a59a18b438
jpinhani <jp.inhani@hotmail.com>
2020-03-22 17:19:32 -0300

    Adjust Revenue CRUD

--- src/app/controllers/receitaController.js
+++ src/app/controllers/receitaController.js
@@ -1,28 +1,176 @@
-const ReceitaModel = require('../models/receitaModel')	
+const ReceitaModel = require('../models/receitaModel')
 
 module.exports = {
-  getReceita(request, response) {   
-     
-    ReceitaModel.getReceita(request.params.id).then(result => {
+
+  getCategory(request, response) {
+    ReceitaModel.getCategory(request.params.idUser).then(result => {
       return response.json(result)
     })
   },
 
-  insertReceita(request, response){
-     ReceitaModel.insertReceita(resquest.body).then(result => {
-      return response.json(result)
-     })
-  },
+  getReceitaAll(request, response) {
+    ReceitaModel.getReceitaAll(request.params.idUser).then(result => {
 
-  updateReceita(request, response){
-    ReceitaModel.updateReceita(resquest.body).then(result => {
-     return response.json(result)
+      /* Trantado a Data para o Padrão Português*/
+      const novosDados = result.map((data) => {
+        const mm = data.DT_PREVISTO.getMonth() + 1;
+        const dd = data.DT_PREVISTO.getDate();
+        const yyyy = data.DT_PREVISTO.getFullYear();
+        const dataNova = dd + '/' + mm + '/' + yyyy
+
+        /* Trantado o VL_PREVISTO para o Padrão Real*/
+        const vlPrevisto = data.VL_PREVISTO;
+        const formatter = new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL'
+        });
+
+        const novoPrevisto = formatter.format(vlPrevisto)
+        const Trat1 = novoPrevisto.toString().replace(".", " ")
+        const Trat2 = Trat1.toString().replace(",", ".")
+        const novoVlPrevisto = Trat2.toString().replace(" ", ",")
+
+        const novaData = {
+          ID: data.ID,
+          ID_GRUPO: data.ID_GRUPO,
+          ID_USER: data.ID_USER,
+          ID_CATEGORIA: data.ID_CATEGORIA,
+          DESCR_CATEGORIA: data.DESCR_CATEGORIA,
+          DESCR_RECEITA: data.DESCR_RECEITA,
+          NUM_PARCELA: data.NUM_PARCELA,
+          VL_PREVISTO: novoVlPrevisto,
+          VL_PREVISTO2: data.VL_PREVISTO,
+          DT_PREVISTO: data.DT_PREVISTO,
+          STATUS: data.STATUS,
+          DATANOVA: dataNova
+        }
+
+        return novaData
+      });
+      return response.json(novosDados)
     })
   },
 
- deleteReceita(request, response){
-  ReceitaModel.deleteReceita(resquest.body).then(result => {
-   return response.json(result)
-  })
- }
+  insertReceita(request, response) {
+    const numparcelas = request.body.parcela
+    let linhas = 0
+    let erro = 0
+    let num = 1
+    if (numparcelas > 1) {
+
+      for (let par = 0; par < numparcelas; par++) {
+        let date = Moment(request.body.dataPrevista)
+        let day = request.body.dayValue
+        if (par > 0) {
+          if (request.body.tipoParcela === 1) {
+            date.add(1, 'month');
+          } else if (request.body.tipoParcela === 2) {
+            date.add(15, 'days');
+          } else {
+            date.add(day, 'days');
+          }
+        }
+
+        request.body.dataPrevista = date.format("YYYY-MM-DD")
+        request.body.parcela = num
+
+        ReceitaModel.insertReceita(request.body).then(result => {
+
+          if (result.status === 200) {
+            linhas = linha + 1
+          } else { erro = erro + 1 }
+
+        })
+        num = num + 1
+      }
+    } else {
+
+      ReceitaModel.insertReceita(request.body).then(result => {
+        return response.json(result)
+      })
+    } if (erro === 0) {
+      return response.json(200)
+    } else {
+      return response.json(erro)
+    }
+  },
+
+
+
+  async updateReceita(request, response) {
+    request.body.id = request.params.id
+    let linhas = 0
+    let erro = 0
+    let numRegistros = 0
+    if (request.body.valueEdit === 'Apenas essa parcela será alterada') {
+
+      ReceitaModel.updateReceita(request.body).then(result => {
+        return response.json(result)
+      })
+    } else {
+      ReceitaModel.selectReceitaGroup(request.body).then(result => {
+
+        numRegistros = result[0].registros
+        ReceitaModel.deleteReceitaGroup(request.body).then(deletes => {
+
+          if (deletes === 'ok') {
+            let num = request.body.parcela
+            numRegistrosNovo = numRegistros + num
+            for (let par = num; par < numRegistrosNovo; par++) {
+              let date = Moment(request.body.dataPrevista)
+              let day = request.body.dayValue
+              if (par > num) {
+                if (request.body.tipoParcela === 1) {
+                  date.add(1, 'month');
+                } else if (request.body.tipoParcela === 2) {
+                  date.add(15, 'days');
+                } else {
+                  date.add(day, 'days');
+                }
+              }
+
+              request.body.dataPrevista = date.format("YYYY-MM-DD")
+              request.body.parcela = par
+
+              ReceitaModel.insertReceita(request.body).then(result => {
+
+                if (result.status === 200) {
+                  linhas = linha + 1
+                } else { erro = erro + 1 }
+
+              })
+              // num = num + 1
+            }
+            if (erro === 0) {
+              return response.json(200)
+            } else {
+              return response.json(erro)
+            }
+          } else {
+            return response.json('Erro ao Deletar')
+          }
+
+        })
+      })
+    }
+  },
+
+  deleteReceita(request, response) {
+    console.log('body', request.body)
+    request.body.id = request.params.id
+    console.log('body esta assim valueEdit', request.body)
+    if (request.body.valueEdit === 'Deletar Receita Selecionada') {
+
+      ReceitaModel.deleteReceita(request.body).then(result => {
+        return response.json(result)
+      })
+
+    } else {
+
+      ReceitaModel.deleteReceitaGroup(request.body).then(result => {
+        return response.json(result)
+      })
+
+    }
+  }
 }
--- src/app/models/receitaModel.js
+++ src/app/models/receitaModel.js
@@ -1,50 +1,129 @@
 const connection = require('../../database/index')
 
 module.exports = {
-  getReceita(receitaId) {
-  	return new Promise((resolve, reject) => {
-			const sql = `SELECT * FROM RECEITA WHERE ID = ${receitaId}`
-      connection.query(sql, function(error, result, fields) {
+
+  getCategory(idUser) {
+    return new Promise((resolve, reject) => {
+      const sql = `SELECT
+                       * FROM CATEGORIA A
+                        WHERE A.ID_USER = '${idUser}'
+                          AND A.ENTRADA = 0`
+      connection.query(sql, function (error, result, fields) {
         if (error)
           reject(error)
         resolve(result)
-			})
+      })
+    })
+  },
+
+  getReceitaAll(idUser) {
+    return new Promise((resolve, reject) => {
+      const sql = `SELECT 
+                      A.ID,
+                      A.ID_GRUPO,
+                      A.ID_USER,
+                      A.ID_CATEGORIA,
+                      B.DESCR_CATEGORIA,
+                      A.DESCR_RECEITA,
+                      A.NUM_PARCELA,
+                      A.VL_PREVISTO,
+                      A.DT_PREVISTO,
+                      A.STATUS 
+                            FROM RECEITA A
+                                LEFT OUTER JOIN CATEGORIA B ON (A.ID_CATEGORIA = B.ID)
+                                LEFT OUTER JOIN CARTAO C ON (A.ID_CARTAO = C.ID)
+                            WHERE A.STATUS IN ('Esperando Pagamento') 
+                              and A.ID_USER = ${idUser}`
+      connection.query(sql, function (error, result, fields) {
+        if (error)
+          reject(error)
+        resolve(result)
+      })
     })
   },
 
   insertReceita(body) {
-    
-  	return new Promise((resolve, reject) => {
-			const sql = `insert into RECEITA values('${body.idUser}','${body.description}', '${body.numParcela}','${body.status}','${body.idNatureza}','${body.vlReal}','${body.vlPrevisto}','${body.dtInicio}','${body.dtReal}','${body.idConta}')`
-      connection.query(sql, function(error, result, fields) {
+    return new Promise((resolve, reject) => {
+      const sql = `INSERT INTO RECEITA VALUES 
+                                    ( null,
+                                     '${body.idGrupo}',
+                                     '${body.idUser}',
+                                     '${body.categoria}',
+                                       null,
+                                     '${body.descrReceita}', 
+                                     '${body.parcela}',
+                                       null,
+                                       null,
+                                     '${body.valorPrevisto}',
+                                     '${body.dataPrevista}',
+                                     '${body.status}')`
+      connection.query(sql, function (error, result, fields) {
         if (error)
           reject(error)
         resolve(result)
-			})
+      })
     })
   },
 
   updateReceita(body) {
-    
-  	return new Promise((resolve, reject) => {
-			const sql = `update RECEITA set DESCRIPTION = '${body.description}', NUM_PARCELA ='${body.numParcela}', STATUS ='${body.status}',ID_NATUREZA ='${body.idNatureza}',VL_REAL ='${body.vlReal}',VL_PREVISTO ='${body.vlPrevisto}',DT_INICIO ='${body.dtInicio}',DT_REAL ='${body.dtReal}',ID_CONTA ='${body.idConta}' where ID='${body.receitaID}' and ID_USER = '${body.idUser}')`
-      connection.query(sql, function(error, result, fields) {
+
+    return new Promise((resolve, reject) => {
+      const sql = `UPDATE RECEITA SET
+                               ID_CATEGORIA = '${body.categoria}',
+                              DESCR_RECEITA = '${body.descrReceita}',
+                                VL_PREVISTO = '${body.valorPrevisto}',
+                                DT_PREVISTO = '${body.dataPrevista}'
+                                  WHERE ID='${body.id}' AND ID_USER = '${body.idUser}'`
+      connection.query(sql, function (error, result, fields) {
         if (error)
           reject(error)
         resolve(result)
-			})
+      })
     })
   },
-  
-  deleteReceita(body) {
-    
-  	return new Promise((resolve, reject) => {
-			const sql = `delete  from RECEITA where ID='${body.receitaId}' and ID_USER = '${body.idUser}')`
-      connection.query(sql, function(error, result, fields) {
+
+  selectReceitaGroup(body) {
+
+    return new Promise((resolve, reject) => {
+      const sql = `SELECT 
+                      COUNT(*) registros FROM RECEITA A 
+                             WHERE A.ID_GRUPO = '${body.idGrupo}'
+                               AND A.ID_USER = '${body.idUser}'
+                               AND A.NUM_PARCELA >= '${body.parcela}'
+                               AND A.STATUS IN ('Esperando Pagamento')`
+      connection.query(sql, function (error, result, fields) {
         if (error)
           reject(error)
         resolve(result)
-			})
+      })
+    })
+  },
+
+  deleteReceitaGroup(body) {
+
+    return new Promise((resolve, reject) => {
+      const sql = `DELETE FROM RECEITA 
+                              WHERE ID_GRUPO = '${body.idGrupo}'
+                                AND ID_USER = '${body.idUser}'
+                                AND NUM_PARCELA >= '${body.parcela}'
+                                AND STATUS IN ('Esperando Pagamento')`
+      connection.query(sql, function (error, result, fields) {
+        if (error)
+          reject(error)
+        resolve('ok')
+      })
+    })
+  },
+
+  deleteReceita(body) {
+
+    return new Promise((resolve, reject) => {
+      const sql = `DELETE FROM RECEITA WHERE ID='${body.id}'`
+      connection.query(sql, function (error, result, fields) {
+        if (error)
+          reject(error)
+        resolve(result)
+      })
     })
   }
 }
