01db41a97a3d313f3f94d3cf6ccb7cf4e318ae59
jpinhani <jp.inhani@hotmail.com>
2020-03-25 17:00:23 -0300

    Configuration Token

--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -1,11 +1,18 @@
 const userModel = require('../models/userModel')
+const jwt = require('jsonwebtoken')
 
 module.exports = {
   async signIn(request, response) {
-
-    const bearerToken = 'BAE39995479EB'
-
     const { email, password } = request.body
+
+    const bearerToken = jwt.sign({
+      emailtoken: email
+    },
+      'BAE39995479EB',
+      {
+        expiresIn: "1h"
+      }
+    )
 
     const user = (await userModel.getUserByEmail(email))[0]
 
