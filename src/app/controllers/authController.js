53387b630a103037022e9197718e70850253fee0
jpinhani <jp.inhani@hotmail.com>
2020-03-25 18:34:26 -0300

    Include TOken in vision 6

--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -4,9 +4,6 @@ const jwt = require('jsonwebtoken')
 module.exports = {
   async signIn(request, response) {
 
-    const id = 1
-    const bearerToken = await jwt.sign({ id }, "BAE39995479EB", { expiresIn: "1h" });
-
     const { email, password } = request.body
 
     const user = (await userModel.getUserByEmail(email))[0]
@@ -16,6 +13,13 @@ module.exports = {
 
     if (user.PASSWORD !== password)
       return response.status(400).json({ message: 'Invalid password' })
+
+
+    const idToken = {
+      id: user.ID,
+      EMAIL: user.EMAIL
+    }
+    const bearerToken = await jwt.sign({ idToken }, "BAE39995479EB", { expiresIn: "1h" });
 
     return response.json({
       user: user,
