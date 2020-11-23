a5c04f8817ee30f40d1d037b9791ed9df9b3cd40
João Inhani <jp.inhani@hotmail.com>
2020-11-23 19:00:34 -0300

    Deploy Initial from Branch Dev

--- src/app/controllers/paygroupController.js
+++ src/app/controllers/paygroupController.js
@@ -7,7 +7,6 @@ module.exports = {
     console.log(request.body)
    const verify = request.body.filter((filtro) => filtro.DataReal === null || filtro.VlReal === null ||  Number.isInteger(filtro.IdCartao) === false)
 
-
     verify.length > 0 ?  response.json({status:400}) :
    request.body.map((item) =>
        paygroupModel.insertLote(item).then(
